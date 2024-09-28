b3ba2063f65cee37d95a178cbe527af88a25267a
riteshkm7 <146635944+riteshkm7@users.noreply.github.com>
2024-09-28 15:52:39 +0530

    multiplication of table

---
+++ Multiplication.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number for which table you want:");
+       
+        int n=sc.nextInt();
+for(int i=1;i<=10;i++){
+    System.out.println(n + "*" + i + " = " + n*i);
+}
+
+
+
+
+    }
+    
+}
