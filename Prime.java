56375dea2291f73cdc8facc0754cb4b677496534
riteshkm7 <146635944+riteshkm7@users.noreply.github.com>
2024-09-28 15:53:36 +0530

    check number is prime or not

---
+++ Prime.java
@@ -0,0 +1,41 @@
+//program to check prime or not
+import java.util.*;
+public class Prime {
+    public static void main(String[] args) {
+     /*   Scanner sc=new Scanner(System.in);
+        System.out.println("enter your number to check prime or not");
+        int num=sc.nextInt();
+        
+        int count=0;
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                count++;
+            }}
+            if(count==2){
+                System.out.println(num+" :is a prime number");
+            }
+            else{
+                System.out.println(num + ":not a prime number");
+            }
+        
+ */
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+    
+
