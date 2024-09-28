31919f28333153458b22d42aeac52d1c0117e3d5
riteshkm7 <146635944+riteshkm7@users.noreply.github.com>
2024-09-28 15:52:04 +0530

    factorial of number

---
+++ Factorial.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number the factorial you want");
+        int num=sc.nextInt();
+        int fact=1;
+        if(num<0){
+            System.out.println("factorial of negative not possible");
+        }
+        else if(num==0){
+            System.out.println("1");
+        }
+        else{
+            for(int i=1;i<=num;i++){
+                fact=fact*i;
+            }
+            System.out.println("factorial is :"+fact);
+
+
+        }
+
+
+
+    }
+    
+}
